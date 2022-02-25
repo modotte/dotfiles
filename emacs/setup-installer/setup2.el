b4b90bad7999b6ab61f2ecfa102fbcc2ee3368dc
modotte <runecroon@gmail.com>
2022-02-25 14:12:10 +0800

    Update emacs config

--- emacs/setup-installer/setup2.el
+++ emacs/setup-installer/setup2.el
@@ -7,29 +7,35 @@
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 
 ;;; CONFIGURATIONS
+(setq auto-save-default nil)
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
 (global-linum-mode)
+(setq show-paren-mode t)
 (add-hook 'prog-mode-hook #'display-fill-column-indicator-mode)
 (electric-pair-mode)
 (smex-initialize)
-(nyan-mode)
 (windmove-default-keybindings)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
 
+
+(yas-global-mode)
 (add-hook 'after-init-hook 'global-company-mode)
 
-;;;; C#
+;; font-size 1/10 pt
+(set-face-attribute 'default nil :height 105)
 
-(eval-after-load
- 'company
- '(add-to-list 'company-backends 'company-omnisharp))
+;;; Haskell
 
-(add-hook 'csharp-mode-hook #'company-mode)
+(add-hook 'haskell-mode-hook #'lsp)
+(add-hook 'haskell-literate-mode-hook #'lsp)
 
-;;;;
+;;;
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -41,10 +47,15 @@
    '("d9646b131c4aa37f01f909fbdd5a9099389518eb68f25277ed19ba99adeb7279" default))
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(yaml-mode helm-swoop fstar-mode csproj-mode gemini-mode lsp-python-ms json-mode nyan-mode lsp-ui smex vlf neotree lsp-mode company-shell emojify web-mode eglot-fsharp monokai-theme omnisharp material-theme)))
+   '(rustic guix geiser-guile emojify-logos magit geiser-racket geiser tuareg fstar-mode lsp-haskell evil idris-mode magic-latex-buffer auctex json-rpc lsp-treemacs yaml-mode helm-swoop csproj-mode gemini-mode lsp-python-ms json-mode lsp-ui smex vlf neotree lsp-mode company-shell emojify web-mode monokai-theme omnisharp material-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(defun ocaml-setup ()
+  (interactive)
+  (require 'opam-user-setup "~/.emacs.d/opam-user-setup.el"))
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
